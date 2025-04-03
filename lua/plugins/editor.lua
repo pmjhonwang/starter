7dcab7a768cbd80d15ed7d95d8bd34603b4a2355
pmjhonwang <pmjhonwang@gmail.com>
2025-04-03 17:58:43 +0800

    style(editor): lualine

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -42,12 +42,15 @@ return {
           ██╔═══╝ ██║╚██╔╝██║██   ██║██╔══██║██║   ██║██║╚██╗██║██║███╗██║██╔══██║██║╚██╗██║██║   ██║
           ██║     ██║ ╚═╝ ██║╚█████╔╝██║  ██║╚██████╔╝██║ ╚████║╚███╔███╔╝██║  ██║██║ ╚████║╚██████╔╝
           ╚═╝     ╚═╝     ╚═╝ ╚════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝
-
           ]],
+          indent = 2,
+          padding = 1,
         },
         sections = {
           { section = "header" },
-          { section = "keys" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           { section = "startup" },
         },
       },
