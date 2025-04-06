7bbf9d71ec91ceb3262143e8ac143584d767f300
pmjhonwang <pmjhonwang@gmail.com>
2025-04-06 10:02:50 +0800

    style(lualine): update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,31 +2,13 @@ return {
   -- lualine
   {
     "nvim-lualine/lualine.nvim",
-
     config = function()
       local lualine = require("lualine")
       lualine.setup({
         options = {
-          icons_enabled = true,
           theme = "auto",
           component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
-          always_divide_middle = true,
-          always_show_tabline = true,
-          globalstatus = true,
-          refresh = {
-            statusline = 100,
-            tabline = 100,
-            winbar = 100,
-          },
-          sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = { "filename" },
-            lualine_x = { "encoding", "fileformat", "filetype" },
-            lualine_y = { "progress" },
-            lualine_z = { "location" },
-          },
         },
       })
     end,
